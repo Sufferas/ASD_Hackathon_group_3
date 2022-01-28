f7ef791af3ca8215943fbb74b278f9f53ee353b0
Alexander Kühhas <alexander.kuehhas@stud.fh-campuswien.ac.at>
2022-01-28 19:55:50 +0100

    Initial commit

---
+++ src/main/java/com/example/asd_hackathon_group_3/AsdHackathonGroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.asd_hackathon_group_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdHackathonGroup3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdHackathonGroup3Application.class, args);
+    }
+
+}
